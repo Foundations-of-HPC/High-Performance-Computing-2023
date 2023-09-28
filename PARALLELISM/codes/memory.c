05c8c74fd0dc13082fbfb2c43c8d38eeee910a81
Marco Celoria <celoria.marco@gmail.com>
2023-09-28 14:22:50 +0200

    add slurm examples

---
+++ PARALLELISM/codes/memory.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void mallocator(double ** vec, size_t N)
+{
+  *vec=(double *)calloc(N, sizeof(double));
+  if (*vec==NULL) fprintf(stderr, "vec is NULL\n");
+}
+
+int main(int argc, char **argv)
+{
+  double * v ;
+  size_t i, j, m;
+  for (i = 1e3 ; i < 1e8 ; i*=10 ) {
+    m = sizeof(double) * i ;
+    mallocator(&v, i);
+    for (j=0; j<i; j++) {
+      v[j] = 1.; 
+    }
+    free(v);
+    fprintf(stdout, "mem = %llu B \n", m);
+    fflush(stdout);
+    sleep(30);
+    fprintf(stdout, "We have waited 30 seconds\n", m);
+    fflush(stdout);
+  }
+  return 0;
+}
---
+++ PARALLELISM/slurm/README.md
@@ -0,0 +1,16 @@
+This folder contains the following files:
+
+- slurm01.job
+A simple example of a batch script for Slurm jobs
+
+- slurm02_#.job 
+Three jobs showing how to run job steps within a Slurm job and the differences between allocating tasks and nodes 
+
+- slurm03_#.job
+Three jobs showing the importance of specifying walltime and memory requirements 
+
+- slurm04.job
+A simple job showing how to: load modules, compile and run an application within a Slurm job
+
+- slurm05.job
+A simple job showing what happens when we load a module 
